966a4c36d1a984bdf3671de870fcc6f4c93a5587
Tylinn <tgonzalezb@est.utn.ac.cr>
2024-06-02 20:01:09 -0600

    Mantenimiento Usuario - Listo

--- Server/app.js
+++ Server/app.js
@@ -8,7 +8,8 @@ const app = express();
 const prism = new PrismaClient();
 
 //---Archivos de rutas---
-const videojuegoRouter= require("./routes/videojuegoRoutes");
+const usuarioRoutes = require('./routes/usuarioRoutes'); 
+
 
 // Acceder a la configuracion del archivo .env
 dotEnv.config();
@@ -30,7 +31,7 @@ app.use(
 );
 
 //---- Definir rutas ---- 
- app.use("/videojuego/",videojuegoRouter);
+ app.use("/usuario/", usuarioRoutes);
 
 // Servidor
 app.listen(port, () => { 
