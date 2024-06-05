7809d00a93324333ecbb4ecd5872c2578a1b5990
lalfaro <lpipo29@gmail.com>
2024-06-04 19:31:47 -0600

    update - routes evento

--- Server/app.js
+++ Server/app.js
@@ -32,7 +32,7 @@ app.use(
 
 //---- Definir rutas ---- 
  app.use("/usuario/", usuarioRoutes);
- app.use("/evento/", eventoRoutes);
+ app.use("/eventos/", eventoRoutes);
 
 // Servidor
 app.listen(port, () => { 
--- Server/routes/eventoRoutes.js
+++ Server/routes/eventoRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const eventoController = require('./eventoController');
+const eventoController = require('../controllers/eventoController');
 
-router.get('/eventos', eventoController.getAll);
-router.get('/eventos/:id', eventoController.getById);
-router.post('/eventos', eventoController.create);
-router.put('/eventos/:id', eventoController.update);
-router.delete('/eventos/:id', eventoController.delete);
+router.get('/', eventoController.getAll);
+router.get('/:id', eventoController.getById);
+router.post('/crear', eventoController.create);
+router.put('/actualizar/:id', eventoController.update);
+router.delete('/eliminar/:id', eventoController.delete);
 
 module.exports = router;
