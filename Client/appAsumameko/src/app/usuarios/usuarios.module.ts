2eb36554a44ddbda23a589352064f1c99af6ca3b
LSElias <eliasrodguer12@gmail.com>
2024-06-04 19:53:36 -0600

    * Modulo Usuarios / Index Usuario

--- Client/appAsumameko/src/app/usuarios/usuarios.module.ts
+++ Client/appAsumameko/src/app/usuarios/usuarios.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuariosRoutingModule } from './usuarios-routing.module';
+import { UsuarioIndexComponent } from './usuario-index/usuario-index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsuarioIndexComponent
+  ],
   imports: [
     CommonModule,
     UsuariosRoutingModule
