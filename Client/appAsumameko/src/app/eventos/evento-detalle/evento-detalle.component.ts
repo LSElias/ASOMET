6fa58abecd04115d7df9e089fdbc71834ecd263a
MaiGdev <mgomzmu@gmail.com>
2024-06-20 00:10:42 -0600

    Detalle evento
    Detalle del evento sin la tabla de los asociados

--- Client/appAsumameko/src/app/eventos/evento-detalle/evento-detalle.component.ts
+++ Client/appAsumameko/src/app/eventos/evento-detalle/evento-detalle.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/shared/generic.service';
+import { EventoFormComponent } from '../evento-form/evento-form.component';
 
 @Component({
   selector: 'app-evento-detalle',
   templateUrl: './evento-detalle.component.html',
-  styleUrls: ['./evento-detalle.component.css']
+  styleUrls: ['./evento-detalle.component.css'],
 })
 export class EventoDetalleComponent {
+  eventId: number | null = null;
+  datos: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild('eventModal') eventModal!: EventoFormComponent;
+  constructor(
+    private gService: GenericService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id !== null) {
+        this.eventId = +id;
+      }
+    });
+    this.fetch();
+  }
+
+  fetch() {
+    this.gService
+      .list(`eventos/${this.eventId}`)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response: any) => {
+        this.datos = response;
+        console.log(response);
+      });
+  }
 }
